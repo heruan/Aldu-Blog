0b29709a9f45c5b4e6ab9cdf489ee3ba2ea24679
Giovanni Lovato <heruan@aldu.net>
2012-10-01 19:21:41 +0200

    Aldu\Blog

---
+++ Controllers/Article.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Aldu\Blog\Controllers\Article
+ *
+ * AlduPHP(tm) : The Aldu Network PHP Framework (http://aldu.net/php)
+ * Copyright 2010-2012, Aldu Network (http://aldu.net)
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0 Unported license (CC BY-SA 3.0)
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Giovanni Lovato <heruan@aldu.net>
+ * @copyright     Copyright 2010-2012, Aldu Network (http://aldu.net)
+ * @link          http://aldu.net/php AlduPHP(tm) Project
+ * @package       Aldu\Blog\Controllers
+ * @uses          Aldu\Core
+ * @since         AlduPHP(tm) v1.0.0
+ * @license       Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+namespace Aldu\Blog\Controllers;
+use Aldu\Core;
+
+class Article extends Core\Controller
+{
+  
+}
