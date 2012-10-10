5339134584efafa92ff2fe4da4d2b23560cd42f0
Giovanni Lovato <heruan@aldu.net>
2012-10-10 08:11:22 +0200

    Added Term

---
+++ Models/Term.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Aldu\Blog\Models\Term
+ *
+ * AlduPHP(tm) : The Aldu Network PHP Framework (http://aldu.net/php)
+ * Copyright 2010-2012, Aldu Network (http://aldu.net)
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0 Unported license (CC BY-SA 3.0)
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Giovanni Lovato <heruan@aldu.net>
+ * @copyright     Copyright 2010-2012, Aldu Network (http://aldu.net)
+ * @link          http://aldu.net/php AlduPHP(tm) Project
+ * @package       Aldu\Blog\Models
+ * @uses          Aldu\Core
+ * @since         AlduPHP(tm) v1.0.0
+ * @license       Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+namespace Aldu\Blog\Models;
+use Aldu\Core;
+
+class Term extends Core\Locale\Localized
+{
+  protected static $configuration = array(
+    'extensions' => array(
+      'localized' => array(
+        'attributes' => array(
+          'title' => true, 'description' => true
+        )
+      )
+    )
+  );
+  public $title;
+  public $description;
+}
