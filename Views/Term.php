79a9dc1afc5465068d742ea72de799fefd3b711f
Giovanni Lovato <heruan@aldu.net>
2012-10-20 11:16:48 +0200

    Added Term's view and controller

---
+++ Views/Term.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Aldu\Blog\Views\Term
+ *
+ * AlduPHP(tm) : The Aldu Network PHP Framework (http://aldu.net/php)
+ * Copyright 2010-2012, Aldu Network (http://aldu.net)
+ *
+ * Licensed under Creative Commons Attribution-ShareAlike 3.0 Unported license (CC BY-SA 3.0)
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Giovanni Lovato <heruan@aldu.net>
+ * @copyright     Copyright 2010-2012, Aldu Network (http://aldu.net)
+ * @link          http://aldu.net/php AlduPHP(tm) Project
+ * @package       Aldu\Blog\Views
+ * @uses          Aldu\Core
+ * @uses          Aldu\Core\View\Helper
+ * @since         AlduPHP(tm) v1.0.0
+ * @license       Creative Commons Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0)
+ */
+
+namespace Aldu\Blog\Views;
+use Aldu\Blog;
+use Aldu\Core;
+use Aldu\Core\View\Helper;
+
+class Term extends Core\View
+{
+}
